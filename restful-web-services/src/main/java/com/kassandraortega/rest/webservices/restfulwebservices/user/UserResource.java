056e63488e7b82c41f338985c01ddff816783459
KassandraOrtega <78051731+KassandraOrtega@users.noreply.github.com>
2021-03-01 13:26:32 -0600

    Add files via upload

---
+++ restful-web-services/src/main/java/com/kassandraortega/rest/webservices/restfulwebservices/user/UserResource.java
@@ -0,0 +1,89 @@
+package com.kassandraortega.rest.webservices.restfulwebservices.user;
+
+import java.net.URI;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@RestController
+public class UserResource {
+
+	@Autowired
+	private UserDaoService service;
+	
+	// GET/users
+	// retrieveAllUsers
+	
+	@GetMapping("/users")
+	public List<User> retrieveAllUsers(){
+		return service.findAll();
+	}
+	
+	// GET/users/{id}
+	// retrieveUser(int id)
+	
+	@GetMapping("/users/{id}")
+	public EntityModel<User> retrieveUser(@PathVariable int id) {
+		User user = service.findOne(id);
+		
+		if(user == null) {
+			throw new UserNotFoundException("id-" + id);
+		}
+		
+		// "all-users", SERVER_PATH + "/users"
+		// retreiveAllUsers
+		EntityModel<User> resource = EntityModel.of(user);
+		
+		WebMvcLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+		
+		resource.add(linkTo.withRel("all-users"));
+		
+		// HATEOAS
+		
+		return resource;
+	}
+	
+	@DeleteMapping("/users/{id}")
+	public void deleteUser(@PathVariable int id) {
+		User user = service.deleteById(id);
+		
+		if(user == null) {
+			throw new UserNotFoundException("id-" + id);
+		}
+	}
+	
+	// input - details of user
+	// output - CREATED & Return the created URI
+	
+	// HATEOAS
+	
+	@PostMapping("/users")
+	public ResponseEntity<Object> createUser(@Validated @RequestBody User user) {
+		User savedUser = service.save(user);
+		//CREATED
+		// /user/{id}
+		
+		URI location = ServletUriComponentsBuilder
+		.fromCurrentRequest()
+		.path("/{id}")
+		.buildAndExpand(savedUser.getId())
+		.toUri();
+		
+		return ResponseEntity.created(location).build();
+	}
+}
