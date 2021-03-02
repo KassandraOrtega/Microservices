5c037d88c64c4627c9f260b9e6f0c3c64a2d19f0
KassandraOrtega <78051731+KassandraOrtega@users.noreply.github.com>
2021-03-02 14:48:29 -0600

    Add files via upload

---
+++ naming-server/src/test/java/com/kassandraortega/microservices/namingserver/NamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kassandraortega.microservices.namingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
